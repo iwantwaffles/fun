d375c411b54be4d8dfc3aa2c1c122db4fd7794cf
iwantwaffles <magyverwray@gmail.com>
2015-01-29 09:59:20 -0800

    lab

---
+++ Lab3/MyMvcSite.Domain/MyMvcSite.Domain/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMvcSite.Domain.Models
+{
+    public class Post
+    {
+        public int PostID { get; set; }
+        public string Body { get; set; }
+        public string Member { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
