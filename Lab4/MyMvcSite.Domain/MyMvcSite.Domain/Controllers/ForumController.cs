7aec6528783503f4d30dcb1fbdace5362e574844
iwantwaffles <magyverwray@gmail.com>
2015-02-10 00:11:44 -0800

    Lab4

---
+++ Lab4/MyMvcSite.Domain/MyMvcSite.Domain/Controllers/ForumController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyMvcSite.Domain.DAL;
+using MyMvcSite.Domain.Models;
+
+namespace MyMvcSite.Domain.Controllers
+{
+    public class ForumController : Controller
+    {
+        private ForumContext db = new ForumContext();
+
+        // GET: Forum
+        public ActionResult Index()
+        {
+            return View(db.Forums.ToList());
+        }
+
+        // GET: Forum/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Forum forum = db.Forums.Find(id);
+            if (forum == null)
+            {
+                return HttpNotFound();
+            }
+            return View(forum);
+        }
+
+        // GET: Forum/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Forum/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ForumID,Title,Description")] Forum forum)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Forums.Add(forum);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(forum);
+        }
+
+        // GET: Forum/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Forum forum = db.Forums.Find(id);
+            if (forum == null)
+            {
+                return HttpNotFound();
+            }
+            return View(forum);
+        }
+
+        // POST: Forum/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ForumID,Title,Description")] Forum forum)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(forum).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(forum);
+        }
+
+        // GET: Forum/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Forum forum = db.Forums.Find(id);
+            if (forum == null)
+            {
+                return HttpNotFound();
+            }
+            return View(forum);
+        }
+
+        // POST: Forum/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Forum forum = db.Forums.Find(id);
+            db.Forums.Remove(forum);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
